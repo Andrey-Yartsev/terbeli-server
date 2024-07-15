fa0e5c567b9b32559a10b4a22dcf91f685d32edd
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2024-07-15 19:43:04 +0300

    next

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.get('/api/users', (req, res) => {
   res.status(201).json(['old fucker']);
 });
